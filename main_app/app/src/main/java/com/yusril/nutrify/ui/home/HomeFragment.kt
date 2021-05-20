f08c3d1f93f4a693af98dec8fe4356d7d6434cee
yysrll <yusril.uzumaki@gmail.com>
2021-05-20 23:49:10 +0800

    [MD] layouting homepage

--- main_app/app/src/main/java/com/yusril/nutrify/ui/home/HomeFragment.kt
+++ main_app/app/src/main/java/com/yusril/nutrify/ui/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.yusril.nutrify.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.yusril.nutrify.R
 import com.yusril.nutrify.databinding.FragmentHomeBinding
-import com.yusril.nutrify.ui.auth.login.LoginActivity
 
 class HomeFragment : Fragment() {
 
@@ -25,6 +28,41 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        showChart()
+    }
+
+    private fun showChart() {
+        val entries = ArrayList<Entry>()
+
+        entries.add(Entry(0f, 100f))
+        entries.add(Entry(4f, 0f))
+        entries.add(Entry(8f, 800f))
+        entries.add(Entry(12f, 1000f))
+        entries.add(Entry(16f, 120f))
+        entries.add(Entry(20f, 500f))
+        entries.add(Entry(24f, 0f))
+
+        val vl = LineDataSet(entries, "My calories")
+        vl.apply {
+            lineWidth = 3f
+            color = R.color.green_500
+            valueTextColor = R.color.material_on_surface_emphasis_medium
+        }
+
+        binding.lineChart.apply {
+            xAxis.labelRotationAngle = 0f
+            data = LineData(vl)
+            setTouchEnabled(false)
+            setPinchZoom(false)
+            description.text = "Hour"
+            setNoDataText("No data yet!")
+            axisRight.isEnabled = false
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            xAxis.setDrawGridLines(false)
+            axisLeft.setDrawGridLines(false)
+            axisRight.setDrawGridLines(false)
+        }
 
     }
 }
