fbe913bcd68427a56e8836b81df28cd5595ac6ee
mainuly999 <mainuly999@gmail.com>
2021-05-24 00:28:49 +0800

    [MD] Layouting EditProfile Activity

---
+++ main_app/app/src/main/java/com/yusril/nutrify/ui/profile/edtprofile/EditProfileActivity.kt
@@ -0,0 +1,46 @@
+package com.yusril.nutrify.ui.profile.edtprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import com.yusril.nutrify.R
+import com.yusril.nutrify.databinding.ActivityEditProfileBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class EditProfileActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragment.DialogDateListener {
+    private lateinit var binding: ActivityEditProfileBinding
+
+    companion object{
+        private const val DATE_PICKER_TAG = "DatePicker"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnInputBirth.setOnClickListener(this)
+
+        var genderList: Array<String> = resources.getStringArray(R.array.gender)
+        binding.inputGender.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, genderList)
+
+    }
+
+    override fun onClick(v: View){
+        when(v.id){
+            R.id.btn_input_birth ->{
+                val datePickerFragment = DatePickerFragment()
+                datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
+            }
+        }
+    }
+
+    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int){
+        val calendar = Calendar.getInstance()
+        calendar.set(year,month,dayOfMonth)
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        binding.tvBirthDate.text = dateFormat.format(calendar.time)
+    }
+}
