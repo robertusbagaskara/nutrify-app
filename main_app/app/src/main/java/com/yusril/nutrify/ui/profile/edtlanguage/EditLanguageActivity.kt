f0177332b2fb365fda0b0030eab2ba00d322eec2
mainuly999 <mainuly999@gmail.com>
2021-05-24 22:35:25 +0800

    [MD] add icon on edit language layout

--- main_app/app/src/main/java/com/yusril/nutrify/ui/profile/edtlanguage/EditLanguageActivity.kt
+++ main_app/app/src/main/java/com/yusril/nutrify/ui/profile/edtlanguage/EditLanguageActivity.kt
@@ -9,14 +9,16 @@ import com.yusril.nutrify.databinding.ActivityEditLanguageBinding
 class EditLanguageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditLanguageBinding
 
+    override fun onResume() {
+        super.onResume()
+        var languageList: Array<String> = resources.getStringArray(R.array.languages)
+        val adapter = ArrayAdapter(this,R.layout.drop_down_item, languageList)
+        binding.inputLanguage.setAdapter(adapter)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditLanguageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        var languageList: Array<String> = resources.getStringArray(R.array.languages)
-        val adapter = ArrayAdapter(this,R.layout.drop_down_item, languageList)
-
-        binding.inputLanguage.setAdapter(adapter)
     }
 }
